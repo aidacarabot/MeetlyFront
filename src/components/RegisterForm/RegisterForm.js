39794f3649292763b13aaed98f46ef3754ada7b4
aidact3 <aidacarabot.tosar@gmail.com>
2024-11-19 19:38:18 -0400

    Actualizado

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,3 @@
-import { fetchData } from "../../utils/api/fetchData";
-import "./RegisterForm.css";
-
 export const RegisterForm = (onSuccessfulRegister) => {
   const form = document.createElement("form");
   form.className = "register-form";
@@ -12,20 +9,18 @@ export const RegisterForm = (onSuccessfulRegister) => {
     <input type="email" name="email" placeholder="Correo Electrónico" required>
     <input type="password" name="password" placeholder="Contraseña" required>
     <input type="password" name="confirmPassword" placeholder="Confirmar Contraseña" required>
-    <input type="file" name="avatar" accept="image/*">
     <button type="submit">Registrarse</button>
   `;
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(form);
-    const userData = Object.fromEntries(formData.entries());
 
     try {
-      const response = await fetchData("/api/v1/users/register", "POST", userData);
+      const response = await fetchData("/api/v1/users/register", "POST", formData);
       if (response.message === "Usuario registrado exitosamente") {
         alert("Te has registrado correctamente, ya puedes iniciar sesión :)");
-        onSuccessfulRegister(); // Esto mostrará el formulario de login
+        onSuccessfulRegister();
       }
     } catch (error) {
       alert("Error en el registro: " + error.message);
