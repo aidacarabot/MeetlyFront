e8c5abf8e4c96c3576409142a413d7d894e30e61
aidact3 <aidacarabot.tosar@gmail.com>
2024-12-16 20:26:42 -0400

    Background set

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import "./Main.css";
 // Exportamos una función llamada Main que crea un elemento <main>.
 export const Main = () => {
   const main = document.createElement("main"); // Creamos un nuevo elemento HTML <main>.
+  main.className = "background-container"; // Asigna la clase con el fondo.
 
   // Añadimos el elemento <main> recién creado al <body> del documento.
   // Esto asegura que todas las páginas tendrán un contenedor principal donde se insertará su contenido.
