68de4285882ede6c8fe1154475aa7c956d0f3f0f
aidact3 <aidacarabot.tosar@gmail.com>
2024-12-20 00:09:54 -0400

    Diseño header terminado

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,11 +6,14 @@ import "./Home.css";
 export const Home = () => {
   const homeDiv = createPage("home");
 
-  // Botón para crear evento
+  // Botón para crear evento usando Button.js
   const createEventButton = Button(
-    "Crear Evento",
-    "btn-create-event",
-    () => window.navigateTo("/crear-evento")
+    "Crear Evento", // Texto del botón
+    "uiverse-create-event-button", // Clase CSS única
+    () => window.navigateTo("/crear-evento"), // Acción al hacer clic
+    "", // Sin ID específico
+    true, // Usa un <span>
+    "uiverse-button-top" // Clase CSS única para el <span>
   );
 
   homeDiv.appendChild(createEventButton);
